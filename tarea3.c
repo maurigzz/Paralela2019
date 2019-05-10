b943195a7905a63227fb84a9ee4e8fe1d102dc1c
maurigzz <45076297+maurigzz@users.noreply.github.com>
2019-05-09 21:01:15 -0400

    Actualización

--- tarea3.c
+++ tarea3.c
@@ -1,59 +1,68 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <mpi.h>
 
-int numeros_random(){
+/* Función que llena un arreglo de n elementos con numeros aleatorios */
+int llenar_arreglo(int arreglo[tamano_arreglo]){
     srand(time(NULL));
-    int r = rand()%100;
-    return r;
+    for (int i = 0 ; i < tamano_arreglo ; i++){
+      arreglo[i] = rand()%100;
+    }
 }
 
-int crear_sub_vector(int inicio, int fin, int* origen){
-    int* sub_vector;
+int crear_sub_arreglo(int inicio, int fin, int* origen){
+    int* sub_arreglo;
     int tamano;
     int auxiliar=0;
     tamano = fin - inicio;
-    sub_vector = (int*)malloc(size * sizeof(int));
-
+    sub_arreglo = (int*)malloc(tamano * sizeof(int));
+    for (int i = inicio ; i < fin ; i++){
+      *(sub_arreglo+auxiliar) = *(origen+i);
+      auxiliar *= 1;
+    }
+    return sub_arreglo
 }
 
-float promedio(){
-    float promedio;
-    return promedio;
+float promedio(int arreglo[tamano_arreglo]){
+    float suma = 0;
+    for (int i = 0 ; i < tamano_arreglo ; i++)
+    return suma;
 }
 
 int main(int argc, char** argv){
 
-    MPI_Init(NUL,NUL); /* Inicia MPI */
+    MPI_Init(NULL,NULL); /* Inicia MPI */
 
-    int mi_rango; /* Rango del proceso    */
-    int numero_procesos; /* Numero de procesos   */
-    int fuente; /* Rango del que envia  */
+    int mi_rango; /* Rango del proceso */
+    int numero_procesos; /* Numero de procesos */
+    int fuente; /* Rango del que envia */
     int destino; /* Rango del que recibe */
     int tag = 0; /* Etiqueta del mensaje */
     int tamano_arreglo = 100000; /* Tamaño arreglo inicial */
-    int arreglo_original(tamano_arreglo); /* Inicia arreglo */
-    int n_over_p;
+    int arreglo_original[tamano_arreglo]; /* Inicia arreglo */
+    int tamano_sub_arreglo;
+    int* sub_arreglo = NULL;
 
-    llenar_arreglo(arreglo_original);
+    llenar_arreglo(arreglo_original[tamano_arreglo]);
 
-    n_over_p = tamano_arreglo/mi_rango;
+    tamano_sub_arreglo = tamano_arreglo/mi_rango;
 
-
-    MPI_Status estado; /* Entrega el estado al recibir */
-    MPI_Comm_size(MPI_COMM_WORLD, &mi_rango); /* Aveiguamos el rango de nuestro proceso */
+    MPI_Comm_size(MPI_COMM_WORLD, &mi_rango); /* Averiguamos el rango de nuestro proceso */
     MPI_Comm_rank(MPI_COMM_WORLD, &numero_procesos); /* Averiguamos el número de procesos */
 
-    if (mi_rango != 0){
-
+    if (mi_rango != 0){ /* Recibe los datos enviados */
+        MPI_Recv(sub_arreglo,tamano_sub_arreglo,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
     }
 
-    else{ /* mi_rango == 0 , Distribuir datos en n arreglos */
+    else{ /* mi_rango == 0 , Distribuye datos del arreglo en n sub_arreglo */
         for (int i = 1 ; i < mi_rango ; i++){
-            
+            sub_arreglo = crear_sub_arreglo(i*tamano_sub_arreglo, (i*tamano_sub_arreglo)+tamano_sub_arreglo, arreglo_original);
+            MPI_Send(sub_arreglo,tamano_arreglo,MPI_INT,i,0,MPI_COMM_WORLD);
         }
+        sub_arreglo = crear_sub_arreglo(0,tamano_arreglo,arreglo_original);
     }
+
+    MPI_Finalize(); /* Finaliza MPI */
 }
